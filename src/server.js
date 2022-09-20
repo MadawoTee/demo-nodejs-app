ff540c76c97f9b348547479afb99fff5e3844235
Taona <taona@synthesis.co.za>
2022-09-20 14:03:42 +0200

    tte

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello, Welcome to OVATIONS ICASA CI CD 202222 Spt 2022");
+    response.end("Hello, Welcome to OVATIONS ICASA CI 555555 Spt 2022");
 
 });
 
